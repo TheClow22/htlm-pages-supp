e76abe92b2d6d4d736153e105f92e8a6b53c4c6f
TheClow22 <maissane94@gmail.com>
2026-04-16 03:44:46 +0200

    Update races-loader.js

--- races-loader.js
+++ races-loader.js
@@ -2,7 +2,7 @@
   'use strict';
   
   // ⚙️ CONFIGURATION
-  const COMMIT_HASH = '27d5907f50406a0d369add7ccbc0a05caea63b9b'; // ⚠️ REMPLACE PAR TON HASH !
+  const COMMIT_HASH = '84e5ef1082988a823507951c72e34e48577bc7ca'; // ⚠️ REMPLACE PAR TON HASH !
   const JSON_URL = `https://cdn.jsdelivr.net/gh/TheClow22/htlm-pages-supp@${COMMIT_HASH}/races-vanes.json`;
   
   let racesData = {};
